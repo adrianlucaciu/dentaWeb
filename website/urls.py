920dd7058905462d5cc00fee219ef8091d68c04a
adrianlucaciu <lucaciuadrian96@gmail.com>
2021-06-28 22:02:06 +0300

    Added Registration and login page

--- website/urls.py
+++ website/urls.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from .views import register
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -9,4 +10,7 @@ urlpatterns = [
     path('pricing.html', views.pricing, name="pricing"),
     path('service.html', views.service, name="service"),
     path('appointment.html', views.appointment, name="appointment"),
+
+    path("register/", register, name="register"),
+    path('', include("django.contrib.auth.urls")),
 ]
--- website/views.py
+++ website/views.py
@@ -1,72 +1,92 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.core.mail import send_mail
 
+from .forms import RegisterForm
+
+
+# Create your views here.
+def register(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect("home")
+    else:
+        form = RegisterForm()
+
+    return render(response, "register.html", {"form": form})
+
+
 def home(request):
-	return render(request, 'home.html', {})
+    return render(request, 'home.html', {})
+
 
 def news(request):
-	return render(request, 'news.html', {})
+    return render(request, 'news.html', {})
+
 
 def contact(request):
-	if request.method == "POST":
-		message_name = request.POST['message-name']
-		message_email = request.POST['message-email']
-		message = request.POST['message']
+    if request.method == "POST":
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
 
-		# send an email
-		send_mail(
-			message_name, # subject
-			message, # message
-			message_email, # from email
-			['lucaciuadrian96@gmail.com'], # To Email
-			)
+        # send an email
+        send_mail(
+            message_name,  # subject
+            message,  # message
+            message_email,  # from email
+            ['lucaciuadrian96@gmail.com'],  # To Email
+        )
 
-		return render(request, 'contact.html', {'message_name': message_name})
+        return render(request, 'contact.html', {'message_name': message_name})
 
-	else:
-		return render(request, 'contact.html', {})
+    else:
+        return render(request, 'contact.html', {})
 
 
 def about(request):
-	return render(request, 'about.html', {})
+    return render(request, 'about.html', {})
+
 
 def pricing(request):
-	return render(request, 'pricing.html', {})
+    return render(request, 'pricing.html', {})
+
 
 def service(request):
-	return render(request, 'service.html', {})
+    return render(request, 'service.html', {})
 
 
 def appointment(request):
-	if request.method == "POST":
-		your_name = request.POST['your-name']
-		your_phone = request.POST['your-phone']
-		your_email = request.POST['your-email']
-		your_address = request.POST['your-address']
-		your_schedule = request.POST['your-schedule']
-		your_date = request.POST['your-date']
-		your_message = request.POST['your-message']
-		
-		# send an email
-		appointment = "Name: " + your_name + " Phone: " + your_phone + " Email: " + your_email + " Address: " + your_address + " Schedule: " + your_schedule + " Day: " + your_date + " Message: " + your_message
+    if request.method == "POST":
+        your_name = request.POST['your-name']
+        your_phone = request.POST['your-phone']
+        your_email = request.POST['your-email']
+        your_address = request.POST['your-address']
+        your_schedule = request.POST['your-schedule']
+        your_date = request.POST['your-date']
+        your_message = request.POST['your-message']
 
-		send_mail(
-			'Appointment Request', # subject
-			appointment, # message
-			your_email, # from email
-			['lucaciuadrian96@gmail.com'], # To Email
-			)
-		
-		return render(request, 'appointment.html', {
-			'your_name': your_name,
-			'your_phone': your_phone,
-			'your_email': your_email,
-			'your_address': your_address,
-			'your_schedule': your_schedule,
-			'your_date': your_date,
-			'your_message': your_message
-			})
+        # send an email
+        appointment = "Name: " + your_name + " Phone: " + your_phone + " Email: " + your_email + " Address: " + your_address + " Schedule: " + your_schedule + " Day: " + your_date + " Message: " + your_message
 
-	else:
-		return render(request, 'home.html', {})
+        send_mail(
+            'Appointment Request',  # subject
+            appointment,  # message
+            your_email,  # from email
+            ['lucaciuadrian96@gmail.com'],  # To Email
+        )
 
+        return render(request, 'appointment.html', {
+            'your_name': your_name,
+            'your_phone': your_phone,
+            'your_email': your_email,
+            'your_address': your_address,
+            'your_schedule': your_schedule,
+            'your_date': your_date,
+            'your_message': your_message
+        })
+
+    else:
+        return render(request, 'home.html', {})
